a6af046fed58b8c3737a3ee003c35ec07974d590
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-03 21:39:26 -0400

    feat: tailwind classes and font setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,41 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsla(var(--border))",
+        input: "hsla(var(--input))",
+        ring: "hsla(var(--ring))",
+        background: "hsla(var(--background))",
+        foreground: "hsla(var(--foreground))",
+        primary: {
+          DEFAULT: "hsla(var(--primary))",
+          foreground: "hsla(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsla(var(--secondary))"
+        },
+        pink: {
+          DEFAULT: "hsla(var(--heading-pink))"
+        },
+        blue: {
+          DEFAULT: "hsla(var(--heading-blue))"
+        },
+        orange: {
+          DEFAULT: "hsla(var(--heading-orange))"
+        },
+        dark: {
+          DEFAULT: "hsla(var(--heading-dark))"
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+        heading: ["var(--font-heading)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
